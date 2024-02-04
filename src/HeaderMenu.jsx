94920b760ea575b1d964ad702e75689144464e60
Anthony Zheng <ant39844@gmail.com>
2024-02-04 00:23:50 -0600

    e
    e

--- src/HeaderMenu.jsx
+++ src/HeaderMenu.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home.jsx';
-import ShowRecipe from './components/ShowRecipe.jsx';
+import RecipeList from './RecipeList.jsx'
 import  "./HeaderMenu.css";
 
 export function HeaderMenu(){
@@ -12,12 +12,12 @@ export function HeaderMenu(){
             <h1>CookBook</h1>
             <nav>
               <Link to="/">Home</Link>
-              <Link to="/ShowRecipe">Recipe List</Link>
+              <Link to="/RecipeList">Recipe List</Link>
             </nav>
             </div>
             <Routes>
                 <Route path="/"  element={<Home />} />
-                <Route path="/ShowRecipe" element={<ShowRecipe />} />
+                <Route path="/RecipeList" element={<RecipeList />} />
             </Routes>
         </Router>
         
