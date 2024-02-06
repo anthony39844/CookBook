18fda983d46c402ec41c5ac100a0498838bab49c
Anthony Zheng <ant39844@gmail.com>
2024-02-06 17:20:05 -0600

    00

--- src/Recipe.jsx
+++ src/Recipe.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Recipe.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClock } from '@fortawesome/free-regular-svg-icons';
 
